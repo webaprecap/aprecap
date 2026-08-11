5a04330a326a22d6ef294503fc564bcd941cd5bf
Víctor Manuel Aguilera Muñoz <web.aprecap@gmail.com>
2026-08-11 14:53:04 -0400

    fix: configure buildCommand in open-next.config.ts to prevent build recursion

--- web/open-next.config.ts
+++ web/open-next.config.ts
@@ -2,6 +2,7 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare/config";
 
 const config = defineCloudflareConfig({});
+config.buildCommand = "npx next build --webpack";
 config.edgeExternals = [...(config.edgeExternals || []), "sharp", "@img/sharp-win32-x64"];
 
 export default config;
